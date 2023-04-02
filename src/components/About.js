fa77adb846871d698b94376da766caa7545a8b72
nvious7 <nghia7van@gmail.com>
2023-04-02 10:40:53 -0400

    Final Commit

--- src/components/About.js
+++ src/components/About.js
@@ -7,17 +7,51 @@ function About() {
     <>
       <div className="about-container">
         <div className="background-container">
-          <img className="profile-picture" src="/profile-picture@2x.png" alt="Me" />
+          <img
+            className="profile-picture"
+            src="/profile-picture@2x.png"
+            alt="Me"
+          />
           <div className="outer" />
           <div className="middle" />
           <div className="inner">
-            <p>Hello!</p>
+            <p>
+              My name is Keithan Van, preferably Keith! I am a Full-Stack
+              Developer/Software Engineer utilizing multiple technology stacks
+              and languages while being based in Jacksonville, FL. I have quite
+              some years of experience professionally in web development as well
+              as extensive experience working remotely. A fan of minimal design
+              but stress cleanliness and responsiveness above all else.
+              Currently, I am focused on growing my experience professionally
+              and improving my skills.
+              <br />
+              <br />
+              <ul>
+                <li>
+                  Frontend: Figma, JavaScript, TypeScript, React, Vue, Angular,
+                  HTML, CSS, SASS, jQuery, Bootstrap
+                </li>
+                <li>
+                  Backend: PHP, Python, Node.js, Express.js, Nodemon, RESTful
+                  APIs, PostgreSQL, Knex, MongoDB, Mongoose
+                </li>
+                <li>
+                  Tools: Mocha, Chai, Jest, Git, GitHub, Heroku, Vercel, Render,
+                  Netlify, Node Package Manager (NPM), Visual Studio Code
+                </li>
+                <li>
+                  Additional Skills: Object-Oriented Programming, Algorithms,
+                  Data Structures
+                </li>
+                <li>Ongoing: AWS, C#, Next.js, Nuxt.js, GraphQL</li>
+              </ul>
+            </p>
           </div>
         </div>
       </div>
       <Back />
     </>
   );
-};
+}
 
 export default About;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,36 +8,66 @@ function Projects() {
       <div className="projects-container">
         <div className="projects-row">
           <div className="projects-card">
-            <img src="/project-1@2x.png" alt="Project 1" />
-            <div class="middle">
-              <div class="text">View</div>
-            </div>
+            <a
+              href="https://github.com/NVious7/MEAN-Task-Tracker"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src="/project-1@2x.png" alt="Project 1" />
+              <div class="middle">
+                <div class="text">View</div>
+              </div>
+            </a>
           </div>
           <div className="projects-card">
-            <img src="/project-2@2x.png" alt="Project 1" />
-            <div class="middle">
-              <div class="text">View</div>
-            </div>
+            <a
+              href="https://github.com/NVious7/Vue-Map-App"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src="/project-2@2x.png" alt="Project 2" />
+              <div class="middle">
+                <div class="text">View</div>
+              </div>
+            </a>
           </div>
           <div className="projects-card">
-            <img src="/project-3@2x.png" alt="Project 1" />
-            <div class="middle">
-              <div class="text">View</div>
-            </div>
+            <a
+              href="https://github.com/NVious7/Restaurant-Reservation-App"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src="/project-3@2x.png" alt="Project 3" />
+              <div class="middle">
+                <div class="text">View</div>
+              </div>
+            </a>
           </div>
           <div className="projects-card">
-            <img src="/project-4@2x.png" alt="Project 1" />
-            <div class="middle">
-              <div class="text">View</div>
-            </div>
+            <a
+              href="https://github.com/NewDevOnTheBlock/Flashcard-App-Fullstack"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src="/project-4@2x.png" alt="Project 4" />
+              <div class="middle">
+                <div class="text">View</div>
+              </div>
+            </a>
           </div>
         </div>
         <div className="projects-row projects-row2">
           <div className="projects-card">
-            <img src="/project-5@2x.png" alt="Project 1" />
-            <div class="middle">
-              <div class="text">View</div>
-            </div>
+            <a
+              href="https://github.com/NVious7/Recipe-CRUD-App"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src="/project-5@2x.png" alt="Project 5" />
+              <div class="middle">
+                <div class="text">View</div>
+              </div>
+            </a>
           </div>
         </div>
       </div>
